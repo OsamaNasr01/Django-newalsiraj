7826b65a51d700449e61baf83236566f9d7e3036
OsamaNasr01 <osama.nasr.01@gmail.com>
2023-07-12 18:47:35 +0200

    migrating users and making signup function

--- newalsiraj/settings.py
+++ newalsiraj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = 'newalsiraj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'newalsiraj',
+        'USER': 'osamanasr',
+        'PASSWORD': 'O5o51986',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.slug
+    # add any other fields you see as important
