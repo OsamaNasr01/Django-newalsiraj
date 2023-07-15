3b555fd998846493fa71d43148338be87f943235
OsamaNasr01 <osama.nasr.01@gmail.com>
2023-07-15 15:01:52 +0200

    modifying user model and adding it to the admin panel

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.utils.text import slugify
 
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     phone_number = models.CharField(max_length=30)
     email = models.EmailField(max_length=254)
-    slug = models.SlugField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    slug = models.SlugField(unique=True, auto_created=True, null=True)
 
     def __str__(self):
-        return self.slug
-    # add any other fields you see as important
+        return self.first_name + ' ' + self.last_name  
+    
+    
